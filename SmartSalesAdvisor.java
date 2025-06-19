d9ae0f3a886cc7446326ca8e202ce1f101d087c6
letidn <letidn@yahoo.com.mx>
2025-06-19 17:12:46 -0600

    Add files via upload

---
+++ SmartSalesAdvisor.java
@@ -0,0 +1,22 @@
+/*
+ * LABORATORIO DE EVALUACIÓN Y CALIDAD DE SOFTWARE
+ * Centro Universitario UAEM Valle de México
+ * Universidad Autónoma del Estado de México
+ */
+
+//package SmartSalesAdvisor;
+
+/**
+ *
+ * @author Chass
+ */
+public class SmartSalesAdvisor {
+
+    /**
+     * @SmartSalesAdvisor: A Decision Support Tool for Microentrepreneurs 
+     */
+    public static void main(String[] args) {
+      new forms_SE().setVisible(true);
+    }
+    
+}
